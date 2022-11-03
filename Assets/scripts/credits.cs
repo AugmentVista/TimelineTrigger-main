b6c13c1b2c1e4d9e53e899236960545c153ed90f
AugmentVista <AuAgments@gmail.com>
2022-11-03 18:09:10 -0300

    All three elements present
    Animation
    Activation
    Audio

---
+++ Assets/scripts/credits.cs
@@ -0,0 +1,2 @@
+// Sound Effect by <a href="https://pixabay.com/users/studioalivioglobal-28281460/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=124463">StudioAlivioGlobal</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=124463">Pixabay</a>
+
